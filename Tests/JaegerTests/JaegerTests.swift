ec340b8b910c8128cc41ea77323d0a55b31a5ee8
Moritz Lang <hi@slashmo.codes>
2020-09-17 10:56:37 +0200

    [ci] Run unit-tests on GitHub Actions

--- Tests/JaegerTests/JaegerTests.swift
+++ Tests/JaegerTests/JaegerTests.swift
@@ -1,3 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Jaeger Client Swift open source project
+//
+// Copyright (c) YEARS Moritz Lang and the Jaeger Client Swift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import XCTest
 
 final class JaegerTests: XCTestCase {
