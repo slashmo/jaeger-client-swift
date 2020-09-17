9e477fa55b1fe03becc67789c22f736ed4d74a8f
Moritz Lang <hi@slashmo.codes>
2020-09-17 12:41:14 +0200

    Add dependency on W3CTraceContext 📦

--- Package.swift
+++ Package.swift
@@ -8,10 +8,12 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/slashmo/gsoc-swift-tracing.git", .branch("main")),
+        .package(url: "https://github.com/slashmo/swift-w3c-trace-context.git", from: "0.3.0"),
     ],
     targets: [
         .target(name: "Jaeger", dependencies: [
             .product(name: "Tracing", package: "gsoc-swift-tracing"),
+            .product(name: "W3CTraceContext", package: "swift-w3c-trace-context"),
         ]),
         .testTarget(name: "JaegerTests", dependencies: [
             .target(name: "Jaeger"),
