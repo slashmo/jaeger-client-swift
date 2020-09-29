c0673fd0e24b078cc921d27386c3de93e5d90107
Moritz Lang <hi@slashmo.codes>
2020-09-29 15:00:02 +0200

    Update Tracing library 📦

--- Package.swift
+++ Package.swift
@@ -9,14 +9,17 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/slashmo/gsoc-swift-tracing.git", .branch("main")),
         .package(url: "https://github.com/slashmo/swift-w3c-trace-context.git", from: "0.4.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.22.1"),
     ],
     targets: [
         .target(name: "Jaeger", dependencies: [
             .product(name: "Tracing", package: "gsoc-swift-tracing"),
             .product(name: "W3CTraceContext", package: "swift-w3c-trace-context"),
+            .product(name: "NIO", package: "swift-nio"),
         ]),
         .testTarget(name: "JaegerTests", dependencies: [
             .target(name: "Jaeger"),
+            .product(name: "NIOInstrumentation", package: "gsoc-swift-tracing"),
         ]),
     ]
 )
